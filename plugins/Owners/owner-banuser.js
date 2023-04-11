5fa175482e37096323ff04639438d1c72cf2023b
BOTGITBASH <kk0298156@gmail.com>
2023-04-11 20:16:13 +0700

    Agak lain

--- plugins/Owners/owner-banuser.js
+++ plugins/Owners/owner-banuser.js
@@ -1,14 +1,15 @@
 let handler = async (m, { conn, text }) => {
-  if (!text) throw "Siapa yg mau dibanned?🗿";
   let who;
   if (m.isGroup)
     who = m.mentionedJid[0]
-      ? m.mentionedJid
+      ? m.mentionedJid[0]
+      : m.quoted
+      ? m.quoted.sender
       : text.replace(/[^0-9]/g, "") + "@s.whatsapp.net";
   else who = m.chat;
-  if (!who) throw "Tag??";
+  if (!who) throw "Siapa yg mau dibanned?🗿";
   let users = global.db.data.users[who];
-  if(!users) users = global.db.data.users[who] = {};
+  if (!users) users = global.db.data.users[who] = {};
   users.banned = true;
   conn.reply(m.chat, `User: ${who} sukses di banned!!`, m);
 };
