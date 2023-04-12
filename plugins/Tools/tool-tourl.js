128ed3c9975bbff2ffbc8b21cbb9d44b41e6e242
BOTGITBASH <kk0298156@gmail.com>
2023-04-12 14:35:53 +0800

    Agak lain

--- plugins/Tools/tool-tourl.js
+++ plugins/Tools/tool-tourl.js
@@ -5,7 +5,7 @@ let handler = async (m) => {
   let q = m.quoted ? m.quoted : m;
   let mime = (q.msg || q).mimetype || "";
   if (!mime) throw "No media found";
-  let media = await q.download();
+  let media = await q.download(true);
   let isTele = /image\/(png|jpe?g|gif)|video\/mp4/.test(mime);
   let link = await (isTele ? uploadImage : uploadFile)(media);
   m.reply(`📮 *L I N K :*
