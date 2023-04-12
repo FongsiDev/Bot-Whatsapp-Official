835784cfccc995c4579ec81fbed4c720a816ade8
BOTGITBASH <kk0298156@gmail.com>
2023-04-12 20:51:49 +0800

    Agak lain

--- plugins/Tools/tool-readviewonce.js
+++ plugins/Tools/tool-readviewonce.js
@@ -1,13 +1,10 @@
 let handler = async (m, { conn }) => {
   if (!m.quoted) throw "where's message?";
-  if (
-    m.quoted.mtype !== "viewOnceMessageV2"
-  )
-    throw "Itu bukan pesan viewOnce";
+  if (m.quoted.mtype !== "viewOnceMessageV2") throw "Itu bukan pesan viewOnce";
   const msg = await m.quoted?.download?.();
   if (!msg) throw "can't open message!";
-  let media = m.quoted.mediaMessage[m.quoted.mediaType]
-  await conn.sendFile(m.chat, await msg, '', await media.caption, m)
+  let media = m.quoted.mediaMessage[m.quoted.mediaType];
+  await conn.sendFile(m.chat, await msg, "", await media.caption, m);
 };
 
 handler.help = ["readviewonce"];
