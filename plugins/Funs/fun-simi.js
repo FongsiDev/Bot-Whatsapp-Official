6316dcef83185a82d027c60f2a1048278a18adb7
BOTGITBASH <kk0298156@gmail.com>
2023-04-16 04:13:17 +0000

    Agak lain

--- plugins/Funs/fun-simi.js
+++ plugins/Funs/fun-simi.js
@@ -1,8 +1,7 @@
-import fetch from "node-fetch";
-import { simtalk } from "simsimi-api";
+import axios from "axios";
 let handler = async (m, { text, args }) => {
   if (!args[0]) throw `Use example .simi halo`;
-  let api = await simtalk(text, "id");
+  let api = await simtalk(text);
   m.reply(api.message);
 };
 handler.command = ["simi"];
@@ -10,3 +9,15 @@ handler.tags = ["fun"];
 handler.help = ["simi"];
 
 export default handler;
+
+async function simtalk(text) {
+  const params = new URLSearchParams();
+  params.append("text", text);
+  params.append("lc", "id");
+  const { data } = await axios({
+    method: "POST",
+    url: "https://api.simsimi.vn/v2/simtalk",
+    data: params,
+  });
+  return data;
+}
