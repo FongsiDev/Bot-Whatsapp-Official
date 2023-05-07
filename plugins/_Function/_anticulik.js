ed256e1ac08445376575f2ba04b3a57a753c8b60
BOTGITBASH <kk0298156@gmail.com>
2023-05-07 15:40:09 +0000

    Agak lain

--- plugins/_Function/_anticulik.js
+++ plugins/_Function/_anticulik.js
@@ -3,19 +3,18 @@ import fetch from "node-fetch";
 import moment from "moment-timezone";
 export async function all(m) {
   // ketika ada yang invite/kirim link grup di chat pribadi
-  if (!m.messageStubType || !m.isGroup) {
+  if (m.messageStubType || m.isGroup) {
     if (m.messageStubType == 20) {
       let group = m.chat;
       let edtr = `@${m.sender.split`@`[0]}`;
       await this.sendMessage(
-        group,
+        m.chat,
         {
-          text: `Hey ${edtr}, Anda mencoba untuk menambahkan saya ke group ini tetapi maaf.\nBot akan keluar karena tidak ada izin dari pihak owner!\n\nSilahkan chat owner saya https://wa.me/${nomorown} untuk memberi izin.\n\nNOTE: JANGAN NYOLONG NAPA 🗿`,
+          text: `Hey ${edtr}\nAnda mencoba untuk menambahkan saya ke group ini tetapi maaf.\nBot akan keluar karena tidak ada izin dari pihak owner!\n\nSilahkan chat owner saya https://wa.me/${nomorown} untuk memberi izin.\n\nNOTE: JANGAN NYOLONG NAPA 🗿`,
           mentions: [m.sender],
         },
         { quoted: fkontak }
       ).then(async (x) => {
-        await m.reply("Sayonara , , ! (≧ω≦)ゞ", m.chat);
         await conn.groupLeave(group);
       });
     }
