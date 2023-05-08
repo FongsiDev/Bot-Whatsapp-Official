dfaf7248e5fdc605622ca09feccf45f7a72fb037
BOTGITBASH <kk0298156@gmail.com>
2023-05-08 13:58:08 +0000

    Agak lain

--- plugins/Owners/owner-listban.js
+++ plugins/Owners/owner-listban.js
@@ -1,47 +1,26 @@
 let handler = async (m, { jid, conn, usedPrefix }) => {
   let chats = Object.values(global.db.data.chats).filter((x) => x.isBanned);
   let users = Object.values(global.db.data.users).filter((x) => x.banned);
-
+  let txt_c = "";
+  chats.map((ob, i) => {
+    txt_c += `
+│ ${i + 1}. ${ob.isName}
+│ ${ob.jid}\n`.trim();
+  });
+  let txt_u = "";
+  users.map((ob, i) => {
+    txt_u += `
+│ ${i + 1}. ${ob.name}
+│ ${ob.jid}\n`.trim();
+  });
   m.reply(
     `
 ┌ *Daftar Chat Terbanned*
-│ Total : ${chats.length} Chat${
-      chats
-        ? "\n" +
-          chats
-            .map((ob, i) => {
-              `
-│ ${i + 1}. ${
-                conn.getName(ob.jid) == undefined
-                  ? "Unknown"
-                  : conn.getName(ob.jid)
-              }
-│ ${ob.jid}
-`.trim();
-            })
-            .join("\n")
-        : ""
-    }
+│ Total : ${chats.length} Chat${chats ? "\n" + txt_c : ""}
 └────
 
 ┌ *Daftar User Terbanned*
-│ Total : ${users.length} User${
-      users
-        ? "\n" +
-          users
-            .map((ob, i) => {
-              `
-│ ${i + 1}. ${
-                conn.getName(ob.jid) == undefined
-                  ? "Unknown"
-                  : conn.getName(ob.jid)
-              }
-│ ${ob.jid}
-`.trim();
-            })
-            .join("\n")
-        : ""
-    }
+│ Total : ${users.length} User${users ? "\n" + txt_u : ""}
 └────
 `.trim()
   );
