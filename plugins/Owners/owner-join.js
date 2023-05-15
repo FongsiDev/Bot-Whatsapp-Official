75134886d82e0d7fa7475bb6ed66d319a61531ee
BOTGITBASH <kk0298156@gmail.com>
2023-05-15 07:00:28 +0000

    Backup Time 5/15/2023, 2:00:27 PM

--- plugins/Owners/owner-join.js
+++ plugins/Owners/owner-join.js
@@ -3,7 +3,14 @@ let linkRegex = /chat.whatsapp.com\/([0-9A-Za-z]{20,24})( [0-9]{1,3})?/i;
 let handler = async (m, { conn, text, isOwner }) => {
   let [_, code, expired] = text.match(linkRegex) || [];
   if (!code) throw "Link invalid";
-  let res = await conn.groupAcceptInvite(code);
+  let res, isJoin;
+    try {
+        isJoin = true;
+     res = await conn.groupAcceptInvite(code);
+        } catch {
+            res = false;
+            isJoin = false;
+        }
   if (isOwner) {
     if (expired) {
       expired = Math.floor(
