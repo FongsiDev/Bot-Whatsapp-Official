191105cc9b0b328ec059e8cc36431e3338a7afc9
BOTGITBASH <kk0298156@gmail.com>
2023-05-15 23:35:38 +0000

    Backup Time 5/16/2023, 6:35:38 AM

---
+++ plugins/Tools/tool-recolor.js
@@ -0,0 +1,26 @@
+// Credits By https://github.com/FongsiDev
+import { processing } from "../../lib/scrape.js";
+import uploadImage from "../../lib/uploadImage.js";
+let handler = async (m) => {
+  let q = m.quoted ? m.quoted : m;
+  let mime = (q.msg || q).mimetype || "";
+  if (!mime) throw "ʀᴇᴘʟʏ ɢᴀᴍʙᴀʀɴʏᴀ ᴋᴀᴋ (～￣▽￣)～";
+  let media = await q.download(true);
+  await m.reply(global.wait);
+  if (!/image\/(jpe?g|png)/.test(mime)) throw `Mime ${mime} tidak support`;
+  let data = await processing(media, "enhance");
+  let url = await uploadImage((await conn.getFile(data, true)).filename);
+  await conn.sendFile(
+    m.chat,
+    data,
+    "hd.jpg",
+    `ɪɴɪ ᴋᴀᴋ ʜᴀꜱɪʟɴʏᴀヾ(≧▽≦*)ᴏ\nLink: ${url}`,
+    m
+  );
+};
+
+handler.help = ["hd <caption|reply media>"];
+handler.tags = ["tools"];
+handler.command = /^(hd|jernih)$/i;
+
+export default handler;
