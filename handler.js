c0c4d40fafe0396ac32cdbf53f43a7cbd838f72d
BOTGITBASH <kk0298156@gmail.com>
2023-05-17 08:27:52 +0000

    Backup Time 5/17/2023, 3:27:52 PM

--- handler.js
+++ handler.js
@@ -95,6 +95,7 @@ export async function handler(chatUpdate) {
     let plugin;
     for (let name in global.plugins) {
       plugin = global.plugins[name];
+    }
     if (!plugin) return;
     if (plugin.disabled) return;
     let __filename = join(___dirname);
@@ -178,7 +179,7 @@ export async function handler(chatUpdate) {
           return;
       }
       if (typeof plugin !== "function") return;
-    }}
+    }
 
     if (!opts["restrict"])
       if (plugin.tags && plugin.tags.includes("admin")) {
